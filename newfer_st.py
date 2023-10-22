3d33205f90625d30d92b36d83a2fa19246514a3b
Tairone <taironeamaral@bol.com.br>
2023-10-22 12:48:48 -0300

    ajustes streamlit

--- newfer_st.py
+++ newfer_st.py
@@ -4,17 +4,18 @@ import plotly.express as px
 from PIL import Image
 
 # Carreguando o ícone da aba
-favicon = "img/logo_zayon.jpeg"
+favicon = "img/zayon_icon.jpeg"
 
 # Configurações da página Streamlit
-st.set_page_config(page_title="Zayon Data Mine - NewFer", page_icon=favicon, layout="wide")
+st.set_page_config(page_title="Zayon - NewFer", page_icon=favicon, layout="wide")
 
 # Adicionando a logo da empresa
-logo_path = "img/logo.jpeg"  # Caminho real para a logo
+logo_path = "img/logo_zayon.png"  # Substitua pelo caminho real para a sua logo
 logo = Image.open(logo_path)
 
 # Exibindo a logo no Streamlit
-st.image(logo, use_column_width=False, width=100)
+st.image(logo, use_column_width=False, width=250)
+
 # Carregar dados
 caminho_do_arquivo = 'data/dataset.xlsx'
 
@@ -25,13 +26,18 @@ try:
 except Exception as e:
     st.error(f"Erro ao importar dados da tabela '{nome_tabela_1}': {str(e)}")
 
+# Interface do Streamlit
+st.title("Zayon Data Mining - NewFer")
+
+sensibilidade_outlyer = st.slider("Outlyer sensitivity:", min_value=1.0, max_value=7.5, value=4.0)
+
 # Função para remover outliers usando o método IQR
 def remove_outliers(data, column):
     Q1 = data[column].quantile(0.25)
     Q3 = data[column].quantile(0.75)
     IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
+    lower_bound = Q1 - sensibilidade_outlyer * IQR
+    upper_bound = Q3 +  sensibilidade_outlyer * IQR
     return data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
 
 # Remover outliers das colunas relevantes
@@ -45,9 +51,6 @@ pellets = dados_filtered['Product Pellets']
 ddrs_rejects = dados_filtered['DDRS Rejects/Feed']
 sdrs_rejects = dados_filtered['SDRS Rejects/Feed']
 
-# Interface do Streamlit
-st.title("Zayon Data Mine - NewFer")
-
 # Criando DataFrame para Plotly
 df_plotly = pd.DataFrame({'Date': dates, 'Pellets': pellets, 'DDRS Rejects': ddrs_rejects * 100, 'SDRS Rejects': sdrs_rejects * 100})
 
@@ -58,12 +61,12 @@ fig1 = px.scatter(df_plotly, x='Pellets', y=['DDRS Rejects', 'SDRS Rejects'],
                  hover_data=['Date'], trendline='ols',
                  color_discrete_sequence=['#1f77b4', '#ff7f0e'])  # Defina aqui as cores desejadas
 
-# Exibindo o gráfico interativo
-st.plotly_chart(fig1, use_container_width=True)
-
 # Adicionando texto explicativo
 st.text("The scatter plot above shows the evolution of DDRS and SDRS in relation to Product Pellets. "
         "You can click on the legend to hide or show specific variables.")
+
+# Exibindo o gráfico interativo
+st.plotly_chart(fig1, use_container_width=True)
 
 # Carregando a tabela específica em um DataFrame
 nome_tabela_2 = 'DATA SET 2'
@@ -85,9 +88,9 @@ fig2 = px.line(df_selected, x='Time [min]', y=cols_to_plot, title='Time Series V
               labels={'value': 'Value', 'variable': 'Variable'},
               line_shape='linear')
 
-# Exibindo o gráfico
-st.plotly_chart(fig2, use_container_width=True)
-
 # Adicionando texto explicativo abaixo do segundo gráfico
 st.text("The line plot above shows the time series visualization of various process variables. "
         "You can click on the legend to hide or show specific variables.")
+
+# Exibindo o gráfico
+st.plotly_chart(fig2, use_container_width=True)
