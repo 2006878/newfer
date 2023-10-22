bc01d1dcd10cb626aa7f39883345e1d22eba88b8
Tairone <taironeamaral@bol.com.br>
2023-10-22 10:44:01 -0300

    ajustes Streamlit

--- newfer_st.py
+++ newfer_st.py
@@ -1,9 +1,21 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
+from PIL import Image
 
-# Especifique o caminho do arquivo Excel
+# Carreguando o ícone da aba
+favicon = "img/logo_zayon.jpeg"
+
+# Configurações da página Streamlit
+st.set_page_config(page_title="Zayon Data Mine - NewFer", page_icon=favicon, layout="wide")
+
+# Adicionando a logo da empresa
+logo_path = "img/logo.jpeg"  # Caminho real para a logo
+logo = Image.open(logo_path)
+
+# Exibindo a logo no Streamlit
+st.image(logo, use_column_width=False, width=100)
+# Carregar dados
 caminho_do_arquivo = 'data/dataset.xlsx'
 
 # Carregando a tabela específica em um DataFrame
@@ -33,18 +45,25 @@ pellets = dados_filtered['Product Pellets']
 ddrs_rejects = dados_filtered['DDRS Rejects/Feed']
 sdrs_rejects = dados_filtered['SDRS Rejects/Feed']
 
+# Interface do Streamlit
+st.title("Zayon Data Mine - NewFer")
+
 # Criando DataFrame para Plotly
 df_plotly = pd.DataFrame({'Date': dates, 'Pellets': pellets, 'DDRS Rejects': ddrs_rejects * 100, 'SDRS Rejects': sdrs_rejects * 100})
 
 # Criando gráfico interativo com Plotly Express
-fig = px.scatter(df_plotly, x='Pellets', y=['DDRS Rejects', 'SDRS Rejects'],
+fig1 = px.scatter(df_plotly, x='Pellets', y=['DDRS Rejects', 'SDRS Rejects'],
                  labels={'variable': 'Select view', 'value': 'Percentage'},
                  title='Evolution of DDRS and SDRS in relation to Product Pellets (No Outliers)',
                  hover_data=['Date'], trendline='ols',
                  color_discrete_sequence=['#1f77b4', '#ff7f0e'])  # Defina aqui as cores desejadas
 
 # Exibindo o gráfico interativo
-st.plotly_chart(fig)
+st.plotly_chart(fig1, use_container_width=True)
+
+# Adicionando texto explicativo
+st.text("The scatter plot above shows the evolution of DDRS and SDRS in relation to Product Pellets. "
+        "You can click on the legend to hide or show specific variables.")
 
 # Carregando a tabela específica em um DataFrame
 nome_tabela_2 = 'DATA SET 2'
@@ -62,9 +81,13 @@ cols_to_plot = ['Time [min]', 'pressure (mbar)', 'Flow rate [Nm³/h]', 'T SP abo
 df_selected = df[cols_to_plot]
 
 # Criando o gráfico interativo
-fig = px.line(df_selected, x='Time [min]', y=cols_to_plot, title='Time Series Visualization of Process Variables',
+fig2 = px.line(df_selected, x='Time [min]', y=cols_to_plot, title='Time Series Visualization of Process Variables',
               labels={'value': 'Value', 'variable': 'Variable'},
               line_shape='linear')
 
 # Exibindo o gráfico
-st.plotly_chart(fig)
+st.plotly_chart(fig2, use_container_width=True)
+
+# Adicionando texto explicativo abaixo do segundo gráfico
+st.text("The line plot above shows the time series visualization of various process variables. "
+        "You can click on the legend to hide or show specific variables.")
