2ad4d72666188152238067807f765b3cb1b2f542
TAIRONE LEANDRO DO AMARAL <taironeamaral@bol.com.br>
2023-12-11 13:34:59 -0300

    Update new_fer1_st.py

--- new_fer1_st.py
+++ new_fer1_st.py
@@ -29,7 +29,7 @@ def load_data(file):
 if 'uploaded_file' not in st.session_state:
     st.session_state.uploaded_file = None
 
-uploaded_file = file_uploader("Escolha um arquivo xlsx contendo a tabela atualizada", type="xlsx")
+uploaded_file = file_uploader("Choose an xlsx file containing the table to analyze", type="xlsx")
 
 if uploaded_file:
     
